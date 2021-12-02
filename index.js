d7cd327721eaaf6fe1de46df19d5abc2edbb3008
AdamDagi <mugutdinovshiha02@gmail.com>
2021-12-02 15:50:55 -0500

    changed questions

--- index.js
+++ index.js
@@ -2,11 +2,82 @@ const genHTML = require("./utils/generateHTML");
 const fs = require("fs");
 const inquirer = require("inquirer");
 let numberOfWorkers;
-const questionNumberOfWorkers = [
+const managerQue = [
     {
         type: "input",
-        name: "numberOfWorkers",
-        message: "How many workers in your team?"
+        name: "managerName",
+        message: "What is the team manager's name?"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the team manager's id?"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?"
+    },
+    {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the team manager's office number?"
+    }
+];
+
+const workerType = [
+    {
+        type: "list",
+        name: "workerType",
+        message: "Which type of team member would you like to add?",
+        choices: ["Manager", "Engineer", "Intern", "No more"],
+        default: "No more"
+    }
+];
+
+const engineerQue = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is the team manager's name?"
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is the team manager's id?"
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the team manager's email?"
+    },
+    {
+        type: "input",
+        name: "engineerOfficeNumber",
+        message: "What is the team manager's office number?"
+    }
+];
+
+const internQue = [
+    {
+        type: "input",
+        name: "internName",
+        message: "What is the team manager's name?"
+    },
+    {
+        type: "input",
+        name: "internId",
+        message: "What is the team manager's id?"
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "What is the team manager's email?"
+    },
+    {
+        type: "input",
+        name: "internOfficeNumber",
+        message: "What is the team manager's office number?"
     }
 ];
 
@@ -34,25 +105,24 @@ function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (e)=>{console.log(e)});
 };
 
-function initNumberOfWorkers() {
+function start() {
     inquirer
-        .prompt(questionNumberOfWorkers)
-        .then((answers) => {
-            numberOfWorkers = answers.numberOfWorkers;
-            collectInputs();
+        .prompt(managerQue)
+        .then((answersManager) => {
+            // collectInputs();
         });
 };
 
-const collectInputs = async () => {
-    const result = [];
+// const collectInputs = async () => {
+//     const result = [];
 
-    for (let i = 0; i < numberOfWorkers; i++) {
-        const managers = await managersQuestionGroup();
-        // const text = genHTML(result);
-        // writeToFile("index.html", text);
+//     for (let i = 0; i < numberOfWorkers; i++) {
+//         const managers = await managersQuestionGroup();
+//         // const text = genHTML(result);
+//         // writeToFile("index.html", text);
 
-        console.log(managers);
-    };
-};
+//         console.log(managers);
+//     };
+// };
 
-initNumberOfWorkers();
+start();
